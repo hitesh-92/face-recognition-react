57733222c918587086bbfe24167e878944090381
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-25 02:53:04 +0100

    added imageFormLink styling

---
+++ src/components/ImageLinkForm/imageLinkForm.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './imageLinkForm.css';
+
+const ImageLinkForm = () => {
+  return (
+    <div>
+      <p className="f3">
+        {'Faces in pictures may or may not be detected!'}
+      </p>
+
+      <div className='center'>
+        <div className='pa4 center br3 shadow-5 form'>
+          <input className='f4 pa2 w-70 center' type='text' />
+          <br />
+          <button className='grow pointer w-30 f4 ma1 link ph3 pv2 dib white bg-light-purple'>
+            Detect
+          </button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default ImageLinkForm;
