41102cfa646832ce7f8275374dce8ab7e74163e0
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-25 08:02:42 +0100

    App component edit and added state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './components/Navigation/navigation';
 import Logo from './components/Logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/imageLinkForm';
@@ -12,21 +12,35 @@ const particleOps = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles params={particleOps} className='particles' />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*
-        <ImageLinkForm />
-        <FaceRecognition />
-      */}
+class App extends Component{
 
-    </div>
-  );
+  constructor(){
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event)
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles params={particleOps} className='particles' />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} />
+        {/*
+          <FaceRecognition />
+        */}
+
+      </div>
+    );
+  };
+
 }
 
 export default App;
