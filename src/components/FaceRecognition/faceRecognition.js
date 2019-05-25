0416f59715c9184fee39a66703876f5ef964d545
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-25 08:55:23 +0100

    FaceRecognition component and claifai api working

---
+++ src/components/FaceRecognition/faceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+  return (
+    <div className='center'>
+      <img src={'http://lokeshdhakar.com/projects/lightbox2/images/image-3.jpg'} />
+    </div>
+  );
+};
+
+export default FaceRecognition;
