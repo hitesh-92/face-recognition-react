48cc4587b0ff8f12d01edaf0d1de9e3f73b5c6c6
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-25 03:20:55 +0100

    add particle background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Navigation from './components/Navigation/navigation';
 import Logo from './components/Logo/logo';
+import ImageLinkForm from './components/ImageLinkForm/imageLinkForm';
+import Rank from './components/Rank/rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const particleOps = {
+  particles: {
+    number: {value: 100}
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles params={particleOps} className='particles' />
       <Navigation />
       <Logo />
+      <Rank />
+      <ImageLinkForm />
       {/*
         <ImageLinkForm />
         <FaceRecognition />
