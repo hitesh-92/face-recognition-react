2c5c49fb209c31edb30419e982bc699d718816b7
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-25 08:55:36 +0100

    FaceRecognition component and claifai api working

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import Navigation from './components/Navigation/navigation';
 import Logo from './components/Logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/imageLinkForm';
 import Rank from './components/Rank/rank';
+import FaceRecognition from './components/FaceRecognition/faceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
+
+const clarifai = new Clarifai.App({
+ apiKey: 'f22f7817782c4db1b5c24c1cc1b71e6f'
+});
 
 const particleOps = {
   particles: {
-    number: {value: 100}
+    number: {value: 150}
   }
 }
 
@@ -27,6 +33,19 @@ class App extends Component{
 
   onButtonSubmit = () => {
     console.log('submitteddd')
+    clarifai.models.predict(
+      // "a403429f2ddf4b49b307e318f00e528b",
+      Clarifai.COLOR_MODEL,
+      "https://samples.clarifai.com/face-det.jpg"
+    )
+    .then(
+    function(response) {
+      console.log(response)
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
 
   render(){
@@ -38,11 +57,9 @@ class App extends Component{
         <Rank />
         <ImageLinkForm
           onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit} 
+          onButtonSubmit={this.onButtonSubmit}
         />
-        {/*
-          <FaceRecognition />
-        */}
+        <FaceRecognition />
 
       </div>
     );
