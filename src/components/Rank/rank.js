0d3359ee28534152c76772224239ea2b12411f76
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-25 03:17:01 +0100

    add particle background

---
+++ src/components/Rank/rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='white f3'>
+        {'#username, current rank:'}
+      </div>
+
+      <div className='white f1'>
+        {'#rankNo'}
+      </div>
+    </div>
+  );
+};
+
+export default Rank;
