e07b91bc02660d109b274568c7538df92048906f
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-28 22:42:09 +0100

    resetting state on user logout

--- src/App.js
+++ src/App.js
@@ -20,25 +20,26 @@ const particleOps = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+    }
+}
+
 class App extends Component{
 
   constructor(){
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-
-    }
+    this.state = initialState;
   }
 
   loadUser = (userData) => {
@@ -109,8 +110,8 @@ class App extends Component{
   }
 
   onRouteChange = (route) => {
-    if(route === 'signout') this.setState({ isSignedIn: false })
-    else if (route === 'home') this.setState({ isSignedIn: true })
+    if(route === 'signout') this.setState(initialState)
+    else if (route === 'home') this.setState({isSignedIn: true});
 
     this.setState({ route })
   }
